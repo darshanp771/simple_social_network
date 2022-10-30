73beacfe439213c0df18a3596b6e9dbc9a8e9375
darshanp771 <117045150+darshanp771@users.noreply.github.com>
2022-10-30 19:07:40 -0400

    Added simple simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,10 @@
+ 
+class Person:
+    """ A person in the social network
+
+    Attributes: 
+    name(str):person name
+    connections (set of Person): other people in the social network who this person
+    
+    
+    """
